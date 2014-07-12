215842e44c9eaa9de093df8383abe1e52a6145e1
Michal Mottl <michal.mottl@coleurope.eu>
2014-07-12 15:13:55 +0200

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,16 +3,25 @@ require 'spec_helper'
 describe "Static pages" do
 
   
-let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+#let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
 subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
 
   describe "Home page" do
     before {visit root_path}
 
-     
-    it { should have_content('Sample App') }
-    it { should have_title("#{base_title}") }
+     let(:heading) { 'Sample App'}
+     let(:page_title) {''}
+     it_should_behave_like "all static pages"
+
+    #it { should have_content('Sample App') }
+   # it { should have_title("#{base_title}") }
     it { should_not have_title('| Home') }
 
   end
@@ -20,21 +29,51 @@ subject { page }
   describe "Help page" do
     before {visit help_path}
 
-    it { should have_content('Help')}
-    it { should have_title("#{base_title} | Help")}
+     let(:heading) { 'Help'}
+     let(:page_title) {'Help'}
+     it_should_behave_like "all static pages"
+
+
+    #it { should have_content('Help')}
+    #it { should have_title("#{base_title} | Help")}
   end
 
   describe "About page" do
     before {visit about_path}
 
-    it { should have_content('About Us')}
-    it { should have_title("#{base_title} | About Us")}
+     let(:heading) { 'About us'}
+     let(:page_title) {'About us'}
+     it_should_behave_like "all static pages"
+
+
+   # it { should have_content('About Us')}
+   # it { should have_title("#{base_title} | About Us")}
   end
 
   describe "Contact page" do
     before {visit contact_path}
 
-    it { should have_content('Contact')}
-    it { should have_title("#{base_title} | Contact")}
+     let(:heading) { 'Contact'}
+     let(:page_title) {'Contact'}
+     it_should_behave_like "all static pages"
+
+  end
+   # it { should have_selector('h1', text: 'Contact')}
+   # it { should have_title("#{base_title} | Contact")}
+
+it "should have the right links on the layout" do
+  visit root_path
+  click_link "About"
+  expect(page).to have_title(full_title('About us'))
+  click_link "Help"
+  expect(page).to have_title(full_title('Help'))
+  click_link "Contact"
+  expect(page).to have_title(full_title('Contact'))
+  click_link "Home"
+  click_link "Sign up now!"
+  expect(page).to have_title(full_title('Sign up'))
+  click_link "Tango Lingo"
+
+
   end
 end
